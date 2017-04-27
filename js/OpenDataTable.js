cb6c98ce7f02e26103285e499bec64df5bf4c8bb
Saheb Mondal <opendatatable@gmail.com>
2017-04-27 22:47:09 +0530

    Update OpenDataTable.js

--- js/OpenDataTable.js
+++ js/OpenDataTable.js
@@ -31,7 +31,7 @@
  * @license	    http://opensource.org/licenses/MIT	MIT License
  * @link	    http://opendatatable.com/
  * @since	    Version 1.0.0
- * @version     1.0.0
+ * @version     1.0.1
  * @filesource
  */
  
